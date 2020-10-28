92e66a0d4d9afd40026054dad116423429a6aa1b
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2020-10-28 14:13:02 -0400

    Update setup.py

--- build_files/setup.py
+++ build_files/setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setuptools.setup(
     name="django-basic-messages", # Replace with your own username
-    version="1.0.1",
+    version="1.0.3",
     author="Jonathan Morgan",
     author_email="jonathan.morgan.007@gmail.com",
     description="A simple re-usable Django application for sending and receiving messages from within django.",
