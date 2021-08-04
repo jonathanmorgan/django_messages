5205ac76094c61da8e6acb6a8224ff264243254f
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2021-08-03 21:56:52 -0400

    app-level default_auto_field setting.

--- apps.py
+++ apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
 
-
 class DjangoMessagesConfig(AppConfig):
+
     name = 'django_messages'
+
+    default_auto_field = 'django.db.models.AutoField'
+    # if you have lots of rows:
+    # default_auto_field = 'django.db.models.BigAutoField'
+
+#-- END AppConfig class DjangoMessagesConfig --#
